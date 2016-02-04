488be943b0778dfa53901c976eca7ae26e59bd18
Spaum145 <spaum.aleksander@mail.ru>
2016-02-05 00:53:55 +0300

    add config to convert http response to json format

--- server/src/main/java/ru/ast/server/config/WebConfig.java
+++ server/src/main/java/ru/ast/server/config/WebConfig.java
@@ -1,13 +1,19 @@
 package ru.ast.server.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.util.List;
 
 /**
  * @author Fenix
@@ -31,5 +37,28 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setSuffix(".jsp");
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(createGsonHttpMessageConverter());
+		super.configureMessageConverters(converters);
+	}
+
+	/**
+	 * Create and configure Gson message converter
+	 * (used to marshall and unmarshall Java Objects to and from JSON over HTTP)
+	 * @return
+	 */
+	private GsonHttpMessageConverter createGsonHttpMessageConverter() {
+		Gson gson = new GsonBuilder()
+//				.excludeFieldsWithoutExposeAnnotation()
+//				.setDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'")
+				.create();
+
+		GsonHttpMessageConverter gsonConverter = new GsonHttpMessageConverter();
+		gsonConverter.setGson(gson);
+
+		return gsonConverter;
 	}
 }
